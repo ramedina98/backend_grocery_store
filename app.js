80abf28a31c608fcd17cd6872766feb097e44ec4
Ricardo Abraham Medina Martin del Campo <111025121+ramedina98@users.noreply.github.com>
2024-03-25 15:47:50 -0600

    .

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ try {
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
-app.listen(3000, () => {
-    console.log('Server up running in http://localhost:3000/')
+
+const port = process.env.PORT || 3000; 
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server up running in http://localhost:${port}/`)
 });
